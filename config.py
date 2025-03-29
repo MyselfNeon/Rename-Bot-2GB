caf7ad2709b69e41d1d3a082a4e335882cfc63c4
NeonAn <justasneon@gmail.com>
2025-03-29 15:50:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
    
     # database config
-    DB_NAME = os.environ.get("DB_NAME","")     
+    DB_NAME = os.environ.get("DB_NAME","MyselfNeon")     
     DB_URL  = os.environ.get("DB_URL","")
  
     # other configs
